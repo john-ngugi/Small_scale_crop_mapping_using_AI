bdb734d9e5e16a775e2eba075ab59dbec35e6bc1
john-ngugi <johnngugi0407@gmail.com>
2023-08-24 08:02:42 +0300

    adding static folders

--- SSCM/mapper/views.py
+++ SSCM/mapper/views.py
@@ -17,8 +17,21 @@ def index(request):
     ee.Initialize(credentials)
     
     Map = geemap.Map()
+    ROI1=ee.FeatureCollection('users/muthamijohn/kwanza_constituency')
+    #cloud mask function
+    def maskS2clouds(image):
+        return image.updateMask(image.select('QA60').eq(0))
 
+    # #importing the image collection and filtering the date and cloud percentage and mapping the cloud filter function 
+    S2=ee.ImageCollection('COPERNICUS/S2_SR').filter(ee.Filter.calendarRange(2017,2021, 'year')).filter(ee.Filter.calendarRange(7, 9, 'month')).map(maskS2clouds)
+    #creating a single image from the sentinel 2 image collection 
+    image=S2.median().clip(ROI1)
+    S2image=ee.ImageCollection('COPERNICUS/S2_SR').filterDate('2017-01-01','2022-12-30').filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 15)).median().clip(ROI1)
+    Map.addLayer(S2image.select('B4','B3','B2'),{'min': 0 ,"max": 3000},"sentinel image")
+    Map.addLayer(image.select('B4','B3','B2'),{'min': 0 ,"max": 3000},"sentinel image 1")
+    print(image.bandNames().getInfo())
+    print(image)
     context= {
-        'map': Map
+        'map': Map.to_html()
     }
     return render(request,'base.html',context)
